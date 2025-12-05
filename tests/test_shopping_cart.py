dbb6a8f7ce78afb208c69771eb0b4b066d70f49c
kathdiao <katherinejoycediao22@gmail.com>
2025-12-05 15:14:17 +0800

    organize code

--- tests/test_shopping_cart.py
+++ tests/test_shopping_cart.py
@@ -1,16 +1,18 @@
 from source.shopping_cart import add_item
 
 def test_add_item():
-    cart = []
-    add_item(cart, "grapes")
-    assert "grapes" in cart
+    cart = ["guava", "apple", "grapes"]
+    add_item(cart, "mango")
+    assert cart == ["guava", "apple", "grapes", "mango"]
 
 
 def test_remove_item():
     pass
 
+
 def test_remove_nonexistent_item():
     pass
+
 
 def test_display_cart():
     pass
